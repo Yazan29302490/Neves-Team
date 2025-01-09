1f1cf3f6422de0e6181d104632f5f1e0e78545d3
Ahmedmahmoud435e <a60101507@gmail.com>
2025-01-10 01:06:08 +0200

    .

--- DD/script.js
+++ DD/script.js
@@ -2,3 +2,38 @@
 // ===============================================================
 // You are now in the programming file, design section page
 // ===============================================================
+
+
+let myArray = ["Pictures/p2.png", "Pictures/p3.png", "Pictures/p4.png"]
+let buttons = document.querySelectorAll("main div.box-content button");
+let indexA = 0;
+let img =  document.querySelector("main div.box-content img");
+console.log(myArray[0])
+
+buttons.forEach((element, index) => {
+   element.addEventListener("click", () => {
+    if (index === 0) {
+        indexA++;
+        if (indexA === 1) {
+           img.src = myArray[0]
+        } else if (indexA  === 2) {
+            img.src = myArray[1]
+        } else if (indexA  ===3) {
+            img.src = myArray[2]
+        }
+
+    } else {
+        indexA--;
+       if (indexA === 0) {
+        img.src = img.src;
+       } else if (indexA === -1) {
+         img.src = myArray[2];
+       }  else if (indexA === -2) {
+        img.src = myArray[1];
+      }else if (indexA === -3) {
+        img.src = myArray[0];
+      }
+
+    }
+   })
+});
