218effd2aef9bdc4d2f293104edc7fa87ecf7665
Ahmedmahmoud435e <a60101507@gmail.com>
2025-01-10 18:59:25 +0200

    .

--- DD/script.js
+++ DD/script.js
@@ -4,36 +4,20 @@
 // ===============================================================
 
 
-let myArray = ["Pictures/p2.png", "Pictures/p3.png", "Pictures/p4.png"]
-let buttons = document.querySelectorAll("main div.box-content button");
-let indexA = 0;
-let img =  document.querySelector("main div.box-content img");
-console.log(myArray[0])
+let buttons = document.querySelectorAll("main button");
+let videos = document.querySelectorAll("div.box-videos img");
+let indexV = 0;
 
-buttons.forEach((element, index) => {
-   element.addEventListener("click", () => {
-    if (index === 0) {
-        indexA++;
-        if (indexA === 1) {
-           img.src = myArray[0]
-        } else if (indexA  === 2) {
-            img.src = myArray[1]
-        } else if (indexA  ===3) {
-            img.src = myArray[2]
-        }
 
-    } else {
-        indexA--;
-       if (indexA === 0) {
-        img.src = img.src;
-       } else if (indexA === -1) {
-         img.src = myArray[2];
-       }  else if (indexA === -2) {
-        img.src = myArray[1];
-      }else if (indexA === -3) {
-        img.src = myArray[0];
-      }
-
-    }
-   })
+buttons.forEach((Buttons, index) => {
+  Buttons.addEventListener("click", () => {
+    videos.forEach(Video => {
+      Video.classList.remove("active")
+    });
+    indexV++;
+    if (indexV >= videos.length) {
+      indexV = 0;
+    };
+    videos[indexV].classList.add("active");
+  });
 });
